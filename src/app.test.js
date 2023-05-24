e16945bcdb37c7d040ff5f45345c9aaa16eec54a
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-24 13:01:20 +1200

    Created test for orders endpoint when sending POST request

--- src/app.test.js
+++ src/app.test.js
@@ -2,7 +2,6 @@
 const request = require("supertest");
 const app = require('./app');
 
-const endpoint = "/wishlists";
 
 describe("/wishlists endpoint", () => {
     it('should return a new product in the wishlists when we send a POST', async () => {
@@ -11,9 +10,35 @@ describe("/wishlists endpoint", () => {
             "productId": "614abf0a93e8e80ace792ac6"
           }
 
-        const response = await request(app).post(endpoint).send(requestBody);
+        const response = await request(app).post('/wishlists').send(requestBody);
         
         expect(response.status).toBe(expectedStatus)
         expect(response.body).toEqual(expect.objectContaining(requestBody))
     });
+
 });
+
+describe("/orders endpoint", () => {
+    it('should return a new order when we send a POST', async () => {
+        const expectedStatus = 200
+        const requestBody = {
+            "groceries": [
+              {
+                "id": "615915303a9ea9532717f95f",
+                "qty": 2
+              },
+              {
+                "id": "6159153639d0db80bed5ec74",
+                "qty": 1
+              }
+            ]
+          }
+
+        const response = await request(app).post('/orders').send(requestBody);
+        
+        expect(response.status).toBe(expectedStatus)
+        expect(response.body).toEqual(expect.objectContaining(requestBody))
+    });
+
+});
+
