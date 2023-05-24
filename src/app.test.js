6dbd7cfb4c99b2e38c5f8a18dd0101d688023137
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-24 12:36:41 +1200

    Installed npm dependencies and Created a test for wishlists endpoint using POST request

--- src/app.test.js
+++ src/app.test.js
@@ -1,1 +1,19 @@
 // FIXME! Write tests for the POST routes
+const request = require("supertest");
+const app = require('./app');
+
+const endpoint = "/wishlists";
+
+describe("/wishlists endpoint", () => {
+    it('should return a new product in the wishlists when we send a POST', async () => {
+        const expectedStatus = 200
+        const requestBody = {
+            "productId": "614abf0a93e8e80ace792ac6"
+          }
+
+        const response = await request(app).post(endpoint).send(requestBody);
+        
+        expect(response.status).toBe(expectedStatus)
+        expect(response.body).toEqual(expect.objectContaining(requestBody))
+    });
+});
